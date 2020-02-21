befb6e55ce6be827d642047b221e74062589a792
Julien <fjulien819@gmail.com>
2020-02-21 10:31:20 +0100

    Add Button action in list

--- src/AppBundle/Admin/BlogPostAdminController.php
+++ src/AppBundle/Admin/BlogPostAdminController.php
@@ -9,6 +9,8 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -21,27 +23,53 @@ final class BlogPostAdmin extends AbstractAdmin
 
         $formMapper
             ->tab('Post')//Onglet
-                ->with('Content', ['class' => 'col-md-9'])// groupe de contenu
-                    ->add('title', TextType::class)
-                    ->add('body', TextareaType::class)
-                    /*
-                      ->add('category', EntityType::class,
-                  [
-                      'class' => Category::class,
-                      'choice_label' => 'name',
-                  ])
-                    */
-                ->end()// fin groupe de contenu
+
+            ->with('Content', ['class' => 'col-md-9'])// groupe de contenu
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Contenu'
+            ])
+            /*
+              ->add('category', EntityType::class,
+          [
+              'class' => Category::class,
+              'choice_label' => 'name',
+          ])
+            */
+            ->end()// fin groupe de contenu
 
 
-                ->with('Meta data', ['class' => 'col-md-3'])
-                    ->add('category', ModelType::class,
-                        [
-                            'class' => Category::class,
-                            'property' => 'name',
-                        ])
-                ->end()
-            ->end();
+            ->with('Meta data', ['class' => 'col-md-3'])
+            ->add('category', ModelType::class,
+                [
+                    'class' => Category::class,
+                    'property' => 'name',
+                    'label' => 'Catégorie'
+                ])
+
+
+            // "privateNotes" field will be rendered only if the authenticated
+            // user is granted with the "ROLE_ADMIN_MODERATOR" role
+            /*
+            -> add ( 'privateNotes' , null , [], [
+                'role' => 'ROLE_ADMIN_MODERATOR'
+            ])
+            */
+
+            // conditionally add "status" field if the subject already exists
+            // `ifFalse()` is also available to build this kind of condition
+            ->ifTrue($this->hasSubject()) // $this->getSubject() = current entity
+           // -> add( 'status' )
+            ->ifEnd()
+
+
+
+            ->end()
+
+            ->end()
+    ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
@@ -49,20 +77,37 @@ final class BlogPostAdmin extends AbstractAdmin
         //vue list
 
         $datagridMapper->add('title')
-        ->add('category', null, [], EntityType::class, [
-        'class' => Category::class,
-        'choice_label' => 'name',
-    ]);
+            ->add('category', null, [], EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ]);
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
         //vue list
 
-        $listMapper->addIdentifier('title') // Champ qui contient un lien vers la page d'édition
+        $listMapper->addIdentifier('title')// Champ qui contient un lien vers la page d'édition
         ->add('category.name')
-        ->add('draft')
-        ;
+            ->add('draft')
+
+        -> add ( '_action' , null , [
+        'actions' => [
+            'show' => [],
+            'edit' => [],
+            'delete' => [],
+            'testLink' => [ 'template' => 'template/CRUD/testActionLink.html.twig'],
+        ]
+    ])
+
+
+    ;
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection
+            ->add('testLink', $this->getRouterIdParameter().'/test');
     }
 
     public function toString($object)
---
+++ src/AppBundle/Controller/BlogPostController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogPostController extends CRUDController
+{
+    public function testLinkAction($id)
+    {
+        /*
+        dump($this->admin->getSubject());
+        dump($this->admin);
+        dump($id);
+      dump('ok'); die();
+*/
+
+
+        $this->addFlash('sonata_flash_success', 'Test effectué avec succès');
+        return new RedirectResponse($this->admin->generateUrl('list'));
+
+        /*
+        $object = $this->admin->getSubject();
+
+        if (!$object) {
+            throw new NotFoundHttpException(sprintf('unable to find the object with id: %s', $id));
+        }
+
+        // Be careful, you may need to overload the __clone method of your object
+        // to set its id to null !
+        $clonedObject = clone $object;
+
+        $clonedObject->setName($object->getName().' (Clone)');
+
+        $this->admin->create($clonedObject);
+
+        $this->addFlash('sonata_flash_success', 'Cloned successfully');
+
+        return new RedirectResponse($this->admin->generateUrl('list'));
+    */
+
+        }
+}
